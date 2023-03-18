b2f157aa3da2926dcb7a0427a37a90b6a855e008
Mustafa <onat.mustafa@hotmail.ccom>
2023-03-18 19:20:30 -0400

    I did some exercise on my own.

---
+++ src/Exercise/Main.java
@@ -0,0 +1,11 @@
+package Exercise;
+
+public class Main {
+    public static void main(String[] args) {
+           UIControl control = new UIControl();
+           control.disable();
+            System.out.println(control.isEnabled());
+
+
+    }
+}
