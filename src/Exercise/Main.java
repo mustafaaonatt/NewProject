28c255f2915e488a75d5770911f5ad7f6dad1d90
Mustafa <onat.mustafa@hotmail.ccom>
2023-03-18 19:49:25 -0400

    I did some exercise on my own.

--- src/Exercise/Main.java
+++ src/Exercise/Main.java
@@ -2,7 +2,9 @@ package Exercise;
 
 public class Main {
     public static void main(String[] args) {
-           UIControl control = new UIControl();
+          var control = new TextBox();
+           control.setText("hi");
+           control.clear();
            control.disable();
             System.out.println(control.isEnabled());
 
---
+++ src/Exercise/TextBox.java
@@ -0,0 +1,12 @@
+package Exercise;
+
+public class TextBox extends UIControl {
+    private String text = "";
+
+    public void setText(String text) {
+        this.text = text;
+    }
+    public  void clear(){
+        text ="";
+    }
+}
