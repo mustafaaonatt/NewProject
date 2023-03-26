a3e9ef9924cc49f482fe6332ffc93af743ad2c44
Mustafa <monat9494@gmail.com>
2023-03-26 14:41:59 -0400

    I added some class for Exceptions

--- src/exeptions/Account.java
+++ src/exeptions/Account.java
@@ -5,13 +5,14 @@ import java.io.IOException;
 public class Account extends Throwable {
     private float balance;
 
-    public  void deposit( float value ) throws IOException {
-        if (value<= 0 )
+    public void deposit(float value) throws IOException {
+        if (value <= 0)
             throw new IOException();
     }
 
     public void withdraw(float value) throws AccountExceptions {
-        if (value>balance)
-          throw new   AccountExceptions accountExceptions = new AccountExceptions( new InsufficientFundsException());
+        if (value > balance) {
+            AccountExceptions accountExceptions = new AccountExceptions(new InsufficientFundsException());
+        }
     }
 }
