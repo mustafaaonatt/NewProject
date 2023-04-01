e0d788a36636ad663137f0ac77849778cc078124
Mustafa <monat9494@gmail.com>
2023-04-01 17:40:01 -0400

    I added main class and created constructor

--- src/SimpleGame/Main.java
+++ src/SimpleGame/Main.java
@@ -2,6 +2,10 @@ package SimpleGame;
 
 public class Main {
     public static void main(String[] args) {
+        Player1 player = new Player1("Ahbay", "sword", 100);
+        System.out.println(player.getName());
+        System.out.println(player.getWeapon());
+        System.out.println(player.getHealth());
 
     }
 }
