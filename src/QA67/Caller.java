dba17933a04de1cb59af49a5cab4b49fe7b16996
Mustafa <monat9494@gmail.com>
2023-04-03 19:27:43 -0400

    I solved question 67

---
+++ src/QA67/Caller.java
@@ -0,0 +1,12 @@
+package QA67;
+
+public class Caller {
+    private void init(){
+        System.out.println("Initialized");
+    }
+    private void start(){
+        init();
+        System.out.println("Started");
+    }
+}
+
---
+++ src/QA67/TestCall.java
@@ -0,0 +1,9 @@
+package QA67;
+
+public class TestCall {
+    public static void main(String[] args) {
+        Caller c = new Caller();
+     //   c.start();
+        // c.init();
+    }
+}
