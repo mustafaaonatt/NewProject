9d1b4af64c2afe36fae4362de56057f1ba5e2dd4
Mustafa <monat9494@gmail.com>
2023-04-08 14:37:46 -0400

    I create School Management App

---
+++ src/SchoolManagement/SchoolManagement.java
@@ -0,0 +1,16 @@
+package SchoolManagement;
+
+import java.util.ArrayList;
+import java.util.List;
+public interface SchoolManagement {
+    void create();
+    void subMenu();
+    void search();
+    void list();
+
+    void exit();
+    void delete();
+
+    List<Person> teachers = new ArrayList<>(); // This ArrayList is created to store teachers objects.
+    List<Person> student = new ArrayList<>(); // This ArrayList is created to store students objects.
+}
