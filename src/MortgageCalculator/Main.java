3aac4cb29b8ee26085d038d49d394a1806964aa3
Mustafa <monat9494@gmail.com>
2023-04-09 14:13:08 -0400

    I create method and made some changes

--- src/MortgageCalculator/Main.java
+++ src/MortgageCalculator/Main.java
@@ -4,18 +4,38 @@ import java.text.NumberFormat;
 import java.util.Scanner;
 
 public class Main {
+    final static byte monthInYear = 12;
+    final static byte percent = 100;
     public static void main(String[] args) {
 
       int  principal = (int)readNumber("Principal", 1000, 1_000_000);
       float annualInterest = (float)readNumber("Annual Interest Rate: ",1, 30);
-      byte years = (byte)readNumber("Period (Years): ",1,30)
+      byte years = (byte)readNumber("Period (Years): ",1,30);
 
-        double mortgage = calculateMortgage(principal, annualInterest, years);
+        printMortgage(principal, annualInterest, years);
 
-        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
-
-        System.out.println("Mortgage: " + mortgageFormatted);
+        printPaymentSchedule(principal, annualInterest, years);
     }
+
+    private static void printMortgage(int principal, float annualInterest, byte years) {
+        double mortgage = calculateMortgage(principal, annualInterest, years);
+        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
+        System.out.println();
+        System.out.println("MORTGAGE");
+        System.out.println("---------");
+        System.out.println("Monthly Payments: " + mortgageFormatted);
+    }
+
+    private static void printPaymentSchedule(int principal, float annualInterest, byte years) {
+        System.out.println();
+        System.out.println("PAYMENT SCHEDULE");
+        System.out.println("----------------");
+        for(short month = 1; month <= years *monthInYear; month++){
+          double balance  = calculateBalance(principal, annualInterest, years,month);
+          System.out.println(NumberFormat.getCurrencyInstance().format(balance));
+        }
+    }
+
     public static double readNumber(String prompt,double min,double max){
      Scanner scanner = new Scanner(System.in);
      double value;
@@ -31,13 +51,23 @@ public class Main {
         return  value;
     }
     public static double calculateMortgage(int principal, float annualInterest, byte years){
-        final byte monthInYear = 12;
-        final byte percent = 100;
+
        short numberOfPayments = (short) (years * monthInYear);
        float monthlyInterest = annualInterest / percent / monthInYear;
         double mortgage = principal * (monthlyInterest * Math.pow(1 + monthlyInterest,numberOfPayments))
                 / (Math.pow(1+ monthlyInterest,numberOfPayments)-1);
         return mortgage;
 
+    }
+    public static  double calculateBalance(int principal, float annualInterest, byte years,short numberOfPaymentsMade){
+
+        short numberOfPayments = (short) (years * monthInYear);
+        float monthlyInterest = annualInterest / percent / monthInYear;
+
+        double balance = principal
+        *(Math.pow(1+monthlyInterest,numberOfPayments) - Math.pow(1+monthlyInterest,numberOfPaymentsMade))
+                /(Math.pow(1+monthlyInterest,numberOfPayments)-1);
+
+        return balance;
     }
 }
