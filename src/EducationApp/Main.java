b4f8762d768524995cea013b9c102fc38ebf4744
mustafaaonatt <monat9494@gmail.com>
2023-04-28 06:17:49 -0400

    I have created Main Class for Education App and added  attitudes

---
+++ src/EducationApp/Main.java
@@ -0,0 +1,13 @@
+package EducationApp;
+
+import java.util.Scanner;
+
+public class Main {
+    private static Scanner scan = new Scanner(System.in);
+    private static String input = "";
+    private static boolean mainMenu = true;
+    private static boolean stdMenu = false;
+    private static boolean crMenu = false;
+    private static boolean tcMenu = false;
+
+}
